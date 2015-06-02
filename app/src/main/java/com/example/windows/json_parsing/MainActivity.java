097dc55f3de76ef94fa5fc17b570773e51dfba37
Sergio Hernandez <szhernandezg@gmail.com>
2015-06-02 10:31:57 -0500

    JSON V4
    Agregando comentarios al codigo

--- app/src/main/java/com/example/windows/json_parsing/MainActivity.java
+++ app/src/main/java/com/example/windows/json_parsing/MainActivity.java
@@ -30,20 +30,15 @@ public class MainActivity extends ListActivity  {
 
     private ProgressDialog pDialog;
 
-    // URL to get contacts JSON
-    //private static String url = "app.szhernandez.dx.am/materias.json";
-
+    // URL para obtener materias del JSON
     private static String url = "http://app.szhernandez.dx.am/materias2.json";
-
-    // JSON Node names
+    // Nombres de los nodos del JSON
     private static final String TAG_MATERIAS = "materias";
     private static final String TAG_ID = "id";
     private static final String TAG_NAME = "nombre";
     private static final String TAG_PROFESOR = "profesor";
-
-    // JSONArray materias
+    // Arreglo Json para materias
     JSONArray Amaterias = null;
-
     // Hashmap para el ListView
     ArrayList<HashMap<String, String>> materiasList;
 
@@ -55,34 +50,28 @@ public class MainActivity extends ListActivity  {
 
         ListView lv = getListView();
 
-        // Listview on item click listener
+        // Clic listenr al elemento
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                // getting values from selected ListItem
+                // obteniendo valores del elemento seleccionado
                 String idmateria = ((TextView) view.findViewById(R.id.id)).getText().toString();
                 String nombre = ((TextView) view.findViewById(R.id.nombre)).getText().toString();
                 String profesor = ((TextView) view.findViewById(R.id.profesor)).getText().toString();
 
-
-                // Starting single contact activity
-                Intent in = new Intent(getApplicationContext(),
-                        mostrar_materia.class);
+                // Comenzando la actividad indiviidual
+                Intent in = new Intent(getApplicationContext(), mostrar_materia.class);
                 in.putExtra(TAG_ID, idmateria);
                 in.putExtra(TAG_NAME, nombre);
                 in.putExtra(TAG_PROFESOR, profesor);
-
                 startActivity(in);
-
             }
         });
-
         // Calling async task to get json
         new GetMaterias().execute();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -97,12 +86,10 @@ public class MainActivity extends ListActivity  {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -114,61 +101,47 @@ public class MainActivity extends ListActivity  {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            // Showing progress dialog
+            // Mostrar ProgressDialog
             pDialog = new ProgressDialog(MainActivity.this);
-            pDialog.setMessage("Please wait...");
+            pDialog.setMessage(getString(R.string.progress));
             pDialog.setCancelable(false);
             pDialog.show();
-
         }
 
         @Override
         protected Void doInBackground(Void... arg0) {
-            // Creating service handler class instance
+            // Creando un instancia de la clase ServiceHandler
             ServiceHandler sh = new ServiceHandler();
-
-            // Making a request to url and getting response
+            //  Realizando una peticion a la URL del JSON
             String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-
             Log.d("Response: ", "> " + jsonStr);
-
             if (jsonStr != null) {
                 try {
                     JSONObject jsonObj = new JSONObject(jsonStr);
-
-                    // Getting JSON Array node
+                    //Obteniendo nodos del arrego JSON
                     Amaterias = jsonObj.getJSONArray(TAG_MATERIAS);
-
-                    // looping through All Contacts
+                    // Ciclo para obtener todas las materias
                     for (int i = 0; i < Amaterias.length(); i++) {
                         JSONObject c = Amaterias.getJSONObject(i);
-
                         String id = c.getString(TAG_ID);
                         String name = c.getString(TAG_NAME);
                         String profesor = c.getString(TAG_PROFESOR);
-
-
-
-
-                        // tmp hashmap for single contact
+                        //  hashmap temporar para una materia
                         HashMap<String, String> contact = new HashMap<String, String>();
-
-                        // adding each child node to HashMap key => value
+                        // Agregando nodos hijos, HashMap key => value
                         contact.put(TAG_ID, id);
                         contact.put(TAG_NAME, name);
                         contact.put(TAG_PROFESOR, profesor);
-
-
-                        // adding contact to contact list
+                        // Agregando la materia a la lista de materias
                         materiasList.add(contact);
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             } else {
-                Log.e("ServiceHandler", "Couldn't get any data from the url");
+                //En caso de haber problemas para conectar y obtener la URL
+                Log.e("ServiceHandler", "No se pueden encontrar ningunos datos en la url seleccionada");
             }
-
             return null;
         }
 
@@ -186,11 +159,8 @@ public class MainActivity extends ListActivity  {
                     R.layout.list_item, new String[] { TAG_ID, TAG_NAME, TAG_PROFESOR
                   }, new int[] { R.id.id,
                     R.id.nombre,R.id.profesor });
-
             setListAdapter(adapter);
         }
-
     }
-
 }
 //Modificado SZHG
